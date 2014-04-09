89d15b9bb8aaf129776fca3ec7e04b768a149b75
Granger <GrangerAbuhoff@gmail.com>
2014-04-08 22:21:09 -0400

    tidy up console.log messages

--- app/assets/javascripts/back.js
+++ app/assets/javascripts/back.js
@@ -1,4 +1,4 @@
-setInterval(reDo, 10000);
+setInterval(reDo, 1000);
 benchMode = false;
 savedTime = "";
 mattersLength = 0;
@@ -32,21 +32,16 @@ function ups(e, ui) {
   matters.toArray().forEach(function(element, index) {
     recordIndex = element.get('number_on_list');
     if (recordIndex === oldIndex) {
-      gak = "AA";
       newNumber = newIndex;
     } else if ((recordIndex < oldIndex && recordIndex < newIndex) || (recordIndex > oldIndex && recordIndex > newIndex)) {
       newNumber = element.get('number_on_list');
-      gak = "AB";
     } else if (recordIndex > oldIndex && recordIndex < newIndex) {
       newNumber = element.get('number_on_list') - 1;
-      gak = "AC"
     } else {
       newNumber = element.get('number_on_list') + 1;
-      gak = "AD";
     }
     element.set('number_on_list', newNumber);
     element.save();
-    console.log(gak + newNumber + " "+element.get('number_on_list'));
   });
 }
 //matters.find(function(model){return model.get('number_on_list') == '24';});
