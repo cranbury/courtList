c561dcf3f01db078b3f93a8f653df9d7eeaf8439
Granger <GrangerAbuhoff@gmail.com>
2014-05-14 09:55:05 -0400

    delete updates indexes on list

--- app/assets/javascripts/back.js
+++ app/assets/javascripts/back.js
@@ -13,24 +13,24 @@ function init() {
     cursor: 'move',
     containment: 'document',
     start: starts,
-    update: ups
+    update: ups,
+    change: highlight
   } );
   //sorted = $("#sortable").sortable( "serialize", { key: "sort" } );
 }
 
   $(document).bind('pageinit', function() {
-  $('#sortable').sortable( {
-    cursor: 'move',
-    containment: 'document',
-    start: starts,
-    update: ups
-  } );
+    $( "#sortable" ).sortable();
     $( "#sortable" ).disableSelection();
     <!-- Refresh list to the end of sort to have a correct display -->
     $( "#sortable" ).bind( "sortstop", function(event, ui) {
       $('#sortable').listview('refresh');
     });
   });
+
+function highlight(e, ui) {
+  //ui.toggleClass("highlight");
+}
 
 function starts(e, ui) {
   // creates a temporary attribute on the element with the old index
@@ -44,13 +44,26 @@ function ups(e, ui) {
   //console.log(oldIndex); console.log(newIndex);
   //console.log(e.target);
 
+  // domMatters = matters.toArray();
+
+  // var lengthOfList = matters.toArray().length;
+
+  // for (var i = 0; i < lengthOfList; i++) {
+  //   element = domMatters[i];
+  //   location = 1;
+  //   element.set('number_on_list', location);
+  //   element.save();
+  // }
+
   matters.toArray().forEach(function(element, index) {
     recordIndex = element.get('number_on_list');
+    //if there was no movement:
     if (recordIndex === oldIndex) {
       newNumber = newIndex;
+      //else if there was no change 
     } else if ((recordIndex < oldIndex && recordIndex < newIndex) || (recordIndex > oldIndex && recordIndex > newIndex)) {
       newNumber = element.get('number_on_list');
-    } else if (recordIndex > oldIndex && recordIndex < newIndex) {
+    } else if (recordIndex > oldIndex && recordIndex <= newIndex) {
       newNumber = element.get('number_on_list') - 1;
     } else {
       newNumber = element.get('number_on_list') + 1;
@@ -71,6 +84,9 @@ function handleDragStop( event, ui ) {
 }
 
 function reDo() {
+  var sortableLinks = $("li.ui-state-default.row");
+  $(sortableLinks).sortable();
+  var linkOrderData = $(sortableLinks).sortable('serialize');
   if (!benchMode){
     matters.fetch({ reset: true }); }
 };
@@ -145,8 +161,21 @@ var MatterView = Backbone.View.extend({
   },
 
   destroy: function() {
+    var delIndex = parseInt(this.model.attributes.number_on_list);
+
     this.model.destroy();
     this.remove();
+
+    matters.toArray().forEach(function(element, index) {
+    recordIndex = element.get('number_on_list');
+    var updatedIndex = recordIndex;
+    //if there was no movement:
+    if (recordIndex > delIndex) {
+      updatedIndex = recordIndex - 1;
+    }
+    element.set('number_on_list', updatedIndex);
+    element.save();
+  });
   },
 
   render: function() {
